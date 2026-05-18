17c96f578215f219fa06a7996dfecafffbcdccd8
Wanderson Victor <wvictor665@gmail.com>
2026-05-18 08:35:38 -0300

    Ambiente Criado, back inicail pronto

---
+++ app/src/main/java/playfoo/com/MainActivity.kt
@@ -0,0 +1,54 @@
+package playfoo.com
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import playfoo.com.ui.dashboard.DashboardScreen
+import playfoo.com.ui.game.GameScreen
+import playfoo.com.ui.menu.MenuScreen
+import playfoo.com.ui.multiplayer.MultiplayerScreen
+import playfoo.com.ui.perfil.PerfilScreen
+import playfoo.com.ui.theme.FOOmobileTheme
+import playfoo.com.ui.turma.TurmaScreen
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FOOmobileTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    ForcaNavHost()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ForcaNavHost() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "menu") {
+        composable("menu") { MenuScreen(navController) }
+        composable("jogo") {
+            GameScreen(
+                onVoltar = { navController.navigateUp() },
+                onJogarNovamente = { navController.navigate("jogo") }
+            )
+        }
+        composable("multiplayer") { MultiplayerScreen(navController) }
+        composable("turmas") { TurmaScreen(navController) }
+        composable("dashboard") { DashboardScreen(navController) }
+        composable("perfil") { PerfilScreen(navController) }
+    }
+}
