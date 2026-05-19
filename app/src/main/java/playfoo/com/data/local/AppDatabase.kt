4b572b0d213b5e7cee181b423bef38298d2a0554
Wanderson Victor <wvictor665@gmail.com>
2026-05-18 22:40:46 -0300

    Meu primeiro commit

--- app/src/main/java/playfoo/com/data/local/AppDatabase.kt
+++ app/src/main/java/playfoo/com/data/local/AppDatabase.kt
@@ -2,6 +2,6 @@ package playfoo.com.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-
+//mnjk
 @Database(entities = [PalavraEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase()
